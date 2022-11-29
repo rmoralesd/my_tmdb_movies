9499fb2ce8d1f8457ef30ff20749851e53aed806
Ricardo Morales <rmorales8303@gmail.com>
2022-11-29 07:53:56 -0500

    Building search screen - Adding poster image

--- lib/features/movies/presentation/movie_search_screen.dart
+++ lib/features/movies/presentation/movie_search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:my_tmdb_movie/features/movies/domain/tmdb_movie.dart';
 
@@ -53,18 +54,34 @@ class MoviesSearchScreen extends StatelessWidget {
             childCount: movies.length,
             (context, index) {
               final movie = movies[index];
-              return Column(
+              return Row(
                 children: [
-                  Text(
-                    movie.title!,
-                    style: Theme.of(context).textTheme.headline6,
+                  CachedNetworkImage(
+                    imageUrl:
+                        'http://image.tmdb.org/t/p/w92/${movie.posterPath!}',
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  if (movie.releaseDate != null) ...[
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text('Released: ${movie.releaseDate}'),
-                  ]
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title!,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      if (movie.releaseDate != null) ...[
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text('Released: ${movie.releaseDate}'),
+                      ]
+                    ],
+                  ),
                 ],
               );
             },
