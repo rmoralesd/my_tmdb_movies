210de1d0c4650ddc0d9aeb7c1aa553b96bccbc39
Ricardo Morales <rmorales8303@gmail.com>
2022-12-01 14:31:27 -0500

    Preparing the app for riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_tmdb_movie/features/movies/presentation/movie_search_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
